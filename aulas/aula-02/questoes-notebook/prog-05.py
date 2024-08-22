0ffaf8bfe389a391a45d98ef8b62c320ff6b18d3
BrazAc <braz0502002@gmail.com>
2024-08-22 19:55:40 -0300

    adicionando mais um programa

--- aulas/aula-02/questoes-notebook/prog-05.py
+++ aulas/aula-02/questoes-notebook/prog-05.py
@@ -8,22 +8,37 @@ Programa:
 Para verificar a eficiência de seu automóvel, todas as vezes que enche o tanque, um motorista 
 registra os quilômetros percorridos e quantos litros de gasolina foram usados. 
 Para ajudá-lo com a análise da eficiência, escreva um programa com repetição controlada por sentinela que 
-solicite, a cada iteração, os quilometros percorridos e a gasolina gasta. O programa deve calcular e exibir 
-a quilometragem por litro a cada vez que ele encheu o tanque. Além disso, depois de processar todas as entradas, 
+solicite, a cada iteração, os quilometros percorridos e a gasolina gasta. 
+
+O programa deve calcular e exibir a quilometragem por litro a cada vez que ele encheu o tanque. 
+Além disso, depois de processar todas as entradas, 
 o programa deve calcular e exibir a quilometragem por litro média, considerando todas as vezes que ele encheu o tanque.
 """
 
-import random
-
-"""Retorna o dobro do numero"""
-def randon(text, seed = 56, *tuplaDados):
-    random.seed(seed)
-    print(tuplaDados)
-    return random.randrange(1, 7)
-
 def main():
-    min(2, 3, 4, 5, )
-    print(randon("teste", *[1, 2, 3, 4, 5]))
+    kmPercorridos = []
+    gasolinaGasta = []
+    kmPorL = []
+    sentinel = 0
+    counter = 0
+    while sentinel == 0:
+        kmPercorridos.append(float(input("Insert the ammount of Km: "))) 
+        gasolinaGasta.append(float(input("Insert the gas used: ")))
+
+        kmPorL.append(kmPercorridos[counter]/gasolinaGasta[counter])
+        print(f"KmL in this round: {kmPorL[counter]}")
+        counter += 1
+
+        sentinel = int(input("\nInsert 0 to repeat\nInsert other number to stop\n"))
+    print(kmPorL)
+
+    soma = 0
+    for kmL in kmPorL:
+        soma += kmL
+    print(f"Median KmL: {soma/len(kmPorL)}")
+        
+
+        
 
 if __name__ ==  "__main__":
     main()
